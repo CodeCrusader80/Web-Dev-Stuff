c2a8749fdcf801d02e9d475303ced8e9dad7f1c2
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-09-30 16:28:55 +0200

    add some configs for the galaxy

---
+++ galaxy/configuration/galaxyConfig.js
@@ -0,0 +1,16 @@
+export const NUM_STARS = 4000
+export const NUM_ARMS = 4
+export const GALAXY_THICKNESS = 4
+export const CORE_DIST_X = 33
+export const CORE_DIST_Y = 33
+export const OUTER_CORE_X_DIST = 100
+export const OUTER_CORE_Y_DIST = 100
+export const ARM_X_DIST = 100
+export const ARM_Y_DIST = 50
+export const ARM_X_MEAN = 150
+export const ARM_Y_MEAN = 100
+
+export const SPIRAL = 3.0
+export const ARMS = 2.0
+
+export const HAZE_RATIO = 0.5
---
+++ galaxy/configuration/starsDist.js
@@ -0,0 +1,5 @@
+export const stars = {
+    percentage: [77.15, 13.4, 7.5, 3.0, 0.8, 0.22],
+    color: [0x7AB9FF, 0x0051FF, 0xFFFFFF, 0xFFFF9D, 0xFFA500, 0xFF0000],
+    size: [0.8, 0.8, 1.20, 1.52, 2.0, 2.5, 3.5]
+}
